13c92a95729b452381f66987ae590bd1181275a9
aabarbosa <aabarbosa.cs@gmail.com>
2018-12-04 20:13:43 -0300

    add program to compile

--- test/programToCompile.pas
+++ test/programToCompile.pas
@@ -9,7 +9,30 @@ literais (inteiros, string, booleanos).
 
 
 program example;
+begin end;
+
+var
+x : boolean;
+i,j : integer;
+charVar : char;
+teste : real;
+
+procedure p;
+begin end;
+
+procedure c(t:integer);
+begin end;
+
+function fct(fct2:integer):real;
+ 
+begin fct := 3.4*8.9; end;
+
+procedure jj;
+VAR s:real;
+begin s:=fct(23); end;
+
 begin
-    
+    teste := 4*8.987;
+    teste := teste;
+    for charVar:='a' to 't' do;
 end.
-    (*TODO: write down a program that contains all above*)
